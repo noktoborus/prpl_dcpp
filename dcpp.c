b4b4cf99b6123f2a0f65ef28b7dc716dcc79e5b9
Noktoborus <noktoborus@gmail.com>
2011-04-18 05:06:52 +0000

    dcpp: fix convert in send message

--- dcpp.c
+++ dcpp.c
@@ -222,25 +222,32 @@ dcpp_list_users_free (gpointer data, gpointer user_data)
 }
 /* send data */
 inline static void
-dcpp_write (PurpleConnection *gc, char *buffer, size_t size)
+dcpp_write (PurpleConnection *gc, char *buffer, size_t size, int need_conv)
 {
 	char *charset;
 	ssize_t lv;
 	struct dcpp_t *dcpp = gc->proto_data;
 	if (!dcpp || dcpp->fd == -1)
 		return;
-	charset = (char*)purple_account_get_string (gc->account, "charset",
-			"UTF-8");
-	if (g_ascii_strcasecmp ("UTF-8", charset))
+	if (need_conv)
 	{
-		charset = g_convert_with_fallback (buffer, -1, "UTF-8",
-							charset, "?", NULL, NULL, NULL);
-		if (charset)
-			size = strlen (charset);
+		charset = (char*)purple_account_get_string (gc->account, "charset",
+				"UTF-8");
+		if (g_ascii_strcasecmp ("UTF-8", charset))
+		{
+			charset = g_convert_with_fallback (buffer, -1, "UTF-8",
+								charset, "?", NULL, NULL, NULL);
+			if (charset)
+				size = strlen (charset);
+		}
+		else
+			charset = buffer;
 	}
 	else
 		charset = buffer;
 	lv = write (dcpp->fd, charset, size);
+	fprintf (stderr, "write (fd=%d, ptr=%p, size=%u) -> %d '%s'\n", dcpp->fd,
+			(void*)charset, size, lv, charset);
 	if (lv != size)
 		purple_connection_error_reason (gc,
 				PURPLE_CONNECTION_ERROR_NETWORK_ERROR,
@@ -283,13 +290,19 @@ dcpp_input_parse (PurpleConnection *gc, gint source, char *input)
 			message = dcpp_extract_key (&(input[6]), end);
 			if (message)
 			{
+				/* alloc buffer */
 				end = strlen (message) + username_len + 22;
 				buffer = g_new0 (char, end);
-				snprintf (buffer, end, "$Key %s|$ValidateNick %s|",
-						message, username);
-				g_free (message);
+				/* send $Key */
+				snprintf (buffer, end, "$Key %s|", message);
 				end = strlen (buffer);
-				dcpp_write (gc, buffer, end);
+				dcpp_write (gc, buffer, end, 0);
+				/* send $ValidateNick */
+				end = username_len + 22;
+				snprintf (buffer, end, "$ValidateNick %s|", username);
+				dcpp_write (gc, buffer, end, 1);
+				/* free buffer */
+				g_free (message);
 				g_free (buffer);
 			}
 		}
@@ -317,7 +330,7 @@ dcpp_input_parse (PurpleConnection *gc, gint source, char *input)
 						"20%c$.$53687091200$|", username, message, message3,
 						1);
 				end = strlen (buffer);
-				dcpp_write (gc, buffer, end);
+				dcpp_write (gc, buffer, end, 1);
 				g_free (buffer);
 				purple_connection_set_state (gc, PURPLE_CONNECTED);
 				if (convy && PURPLE_CONV_CHAT (convy)->left)
@@ -428,7 +441,7 @@ dcpp_input_parse (PurpleConnection *gc, gint source, char *input)
 					buffer = g_new (char, username_len);
 					snprintf (buffer, username_len, "$MyPass %s|", message3);
 					username_len = strlen (buffer);
-					dcpp_write (gc, buffer, username_len);
+					dcpp_write (gc, buffer, username_len, 1);
 					g_free (buffer);
 				}
 			}
@@ -864,7 +877,7 @@ dcpp_send (PurpleConnection *gc, const char *who, const char *what)
 				username, username, text);
 	}
 	g_free (text);
-	dcpp_write (gc, buffer, text_len);
+	dcpp_write (gc, buffer, text_len, 1);
 	g_free (buffer);
 	return TRUE;
 }
